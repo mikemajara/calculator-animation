1430f2b23e4450f659a2a3ee3c8de255783191e8
Miguel Alcalde <fmiguel05@gmail.com>
2024-07-01 23:10:21 +0200

    animation examples

--- src/app/animations/page.tsx
+++ src/app/animations/page.tsx
@@ -1,5 +1,43 @@
+"use client";
 import AnimatedBall from "@/components/calculator/animated-ball";
+import { useEffect, useReducer } from "react";
 
 export default function Page() {
-  return <AnimatedBall className="ease-[--ease-out-quad]" />;
+  const [clicked, toggleClicked] = useReducer((s) => !s, false);
+  useEffect(() => {
+    clicked && toggleClicked();
+  });
+  return (
+    <div className="flex flex-col gap-2 p-10 ">
+      <button className="border border-black" onClick={toggleClicked}>
+        animate
+      </button>
+      <h1>Control</h1>
+      <AnimatedBall toggleAnimation={clicked} />
+
+      <h2 className="font-bold font-xl">In</h2>
+      <AnimatedBall toggleAnimation={clicked} className="ease-in-quad" />
+      <AnimatedBall toggleAnimation={clicked} className="ease-in-cubic" />
+      <AnimatedBall toggleAnimation={clicked} className="ease-in-quart" />
+      <AnimatedBall toggleAnimation={clicked} className="ease-in-quint" />
+      <AnimatedBall toggleAnimation={clicked} className="ease-in-expo" />
+      <AnimatedBall toggleAnimation={clicked} className="ease-in-circ" />
+
+      <h2 className="font-bold font-xl">Out</h2>
+      <AnimatedBall toggleAnimation={clicked} className="ease-out-quad" />
+      <AnimatedBall toggleAnimation={clicked} className="ease-out-cubic" />
+      <AnimatedBall toggleAnimation={clicked} className="ease-out-quart" />
+      <AnimatedBall toggleAnimation={clicked} className="ease-out-quint" />
+      <AnimatedBall toggleAnimation={clicked} className="ease-out-expo" />
+      <AnimatedBall toggleAnimation={clicked} className="ease-out-circ" />
+
+      <h2 className="font-bold font-xl">In out</h2>
+      <AnimatedBall toggleAnimation={clicked} className="ease-in-out-quad" />
+      <AnimatedBall toggleAnimation={clicked} className="ease-in-out-cubic" />
+      <AnimatedBall toggleAnimation={clicked} className="ease-in-out-quart" />
+      <AnimatedBall toggleAnimation={clicked} className="ease-in-out-quint" />
+      <AnimatedBall toggleAnimation={clicked} className="ease-in-out-expo" />
+      <AnimatedBall toggleAnimation={clicked} className="ease-in-out-circ" />
+    </div>
+  );
 }
--- src/components/calculator/animated-ball.tsx
+++ src/components/calculator/animated-ball.tsx
@@ -1,20 +1,28 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import clsx from "clsx";
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 
-export default function AnimatedBall() {
+export default function AnimatedBall({
+  className = "",
+  toggleAnimation = false,
+  style = {},
+}) {
   const [clicked, toggleClicked] = useReducer((s) => !s, false);
+  useEffect(() => {
+    toggleAnimation && toggleClicked();
+  });
   return (
     <div
-      className={clsx(
-        "w-4 h-4 bg-yellow-300 rounded-full border border-yellow-400 relative inset-0 translate-x-32 ease-[--ease-in-out-quad] transition-all",
+      className={cn(
+        "w-4 h-4 bg-yellow-300 rounded-full border border-yellow-400 relative transition-all duration-1000",
         {
-          "bg-green-400 border-green-500 -translate-x-32": clicked,
-        }
+          "bg-green-400 border-green-500 translate-x-96": clicked,
+        },
+        className
       )}
       onClick={toggleClicked}
+      style={style}
     />
   );
 }
